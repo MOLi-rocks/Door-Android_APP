7edf72f1b3b1da0614e8f03aebb35aa80777a6c9
YY <z3388638@gmail.com>
2017-04-01 21:47:50 +0800

    remove duplicate code

--- app/src/main/java/com/yy/molidoor/MainActivity.java
+++ app/src/main/java/com/yy/molidoor/MainActivity.java
@@ -32,30 +32,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handleOpen(View view) {
-        Toast t = Toast.makeText(MainActivity.this, "open", Toast.LENGTH_LONG);
-        t.show();
-
-        WifiManager wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
-        if (wifi != null){
-            WifiManager.MulticastLock lock = wifi.createMulticastLock("mylock");
-            lock.acquire();
-        }
-
-        try {
-            // send
-            String msg = "{\"event\": \"DOOR_OPEN\"}";
-            InetAddress group = InetAddress.getByName("239.5.6.7");
-            s = new MulticastSocket(12333);
-            s.joinGroup(group);
-            dgramPacket = new DatagramPacket(msg.getBytes(), msg.length(), group, 12332);
-            sendHnadler.post(multicastSend);
-        } catch (IOException e) {
-
-        }
+        setDoor("open");
     }
 
     public void handleClose(View view) {
-        Toast t = Toast.makeText(MainActivity.this, "open", Toast.LENGTH_LONG);
+        setDoor("close");
+    }
+
+    public void setDoor(String action) {
+        Toast t = Toast.makeText(MainActivity.this, action, Toast.LENGTH_LONG);
         t.show();
 
         WifiManager wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
@@ -63,10 +48,10 @@ public class MainActivity extends AppCompatActivity {
             WifiManager.MulticastLock lock = wifi.createMulticastLock("mylock");
             lock.acquire();
         }
-
+        String command = action == "open" ? "DOOR_OPEN" : "DOOR_CLOSE";
         try {
             // send
-            String msg = "{\"event\": \"DOOR_CLOSE\"}";
+            String msg = "{\"event\": \"" + command + "\"}";
             InetAddress group = InetAddress.getByName("239.5.6.7");
             s = new MulticastSocket(12333);
             s.joinGroup(group);
